a5afdd9a4eef1316d8912348f7fdaa56d8c3de6d
Lassi Kortela <lassi@lassi.io>
2021-02-08 00:33:26 +0200

    Add Gauche patch and example

---
+++ example.scm
@@ -0,0 +1,14 @@
+#! /usr/bin/env gosh
+#! vim: tw=60 ts=2 expandtab fileencoding=euc-jp :
+#! Copyright 2018, 2020 Just A. Schemer <schemer@example.org>
+#! SPDX-License-Identifier: GPL-3.0-or-later
+
+(import (scheme base))
+(define (foo) (write-string "Hello\n"))
+
+#! Local Variables:
+#! coding: iso-8859-1
+#! mode: Scheme
+#! comment-column: 0
+#! eval: (font-lock-mode -1)
+#! End:
